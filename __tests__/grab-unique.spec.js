0546d1e62ebc37ff2e8e9f08c8dafc824bd96f4f
Camden_Shaw <camden.shaw@gmail.com>
2017-11-16 21:20:13 -0800

    identifies multiple instances of letters in different parts of the string

--- __tests__/grab-unique.spec.js
+++ __tests__/grab-unique.spec.js
@@ -13,10 +13,10 @@ describe("letter grabber function detector", () => {
     })
     describe("it should remove white spaces", () => {
         it("this functions shouldn't have any spaces in the string, it is a single word counter only.", () => {
-            expect(grabUnique("aaa aajklfjllkaj")).toEqual('j')
+            expect(grabUnique("aaa aajklfjllkaj")).toEqual('l')
             expect(grabUnique("kkaabbbaa    ddeeeeef")).toEqual('f')
             expect(grabUnique(" kkaabbb    ddeeeeef   ")).toEqual('f')
-            expect(grabUnique(" aaa aajklfjllkaj ")).toEqual('j')
+            expect(grabUnique(" aaa aajklfjllkaj ")).toEqual('l')
         })
     })
 })
